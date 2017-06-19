721e2e5e7e11919bd32f1b1be1c745399884e9e5
Makoto Kitamura <today.is.sailingday24@gmail.com>
2017-06-19 20:58:08 +0900

    fix test

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -17,7 +17,7 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create blog" do
     assert_difference('Blog.count') do
-      post blogs_url, params: { blog: { content: @blog.content, img_url: @blog.img_url, title: @blog.title } }
+      post blogs_url, params: { blog: { content: @blog.content, image: @blog.image, title: @blog.title } }
     end
 
     assert_redirected_to blog_url(Blog.last)
@@ -34,7 +34,7 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update blog" do
-    patch blog_url(@blog), params: { blog: { content: @blog.content, img_url: @blog.img_url, title: @blog.title } }
+    patch blog_url(@blog), params: { blog: { content: @blog.content, image: @blog.image, title: @blog.title } }
     assert_redirected_to blog_url(@blog)
   end
 
